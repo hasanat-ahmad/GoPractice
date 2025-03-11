de52e9a5ee9c1697367be6b7fb9c05f870c09fd0
Sahibzada Hasanat Ahmad <sahibzadahasanatahmad@gmail.com>
2025-03-11 17:43:42 +0500

    changes added

--- main.go
+++ main.go
@@ -1,16 +1,6 @@
 package main
 
 func main() {
-	var arr [5]int
-
-	InsertElement(arr[:], 10)
-	InsertElement(arr[:], 5)
-	InsertElement(arr[:], 71)
-	InsertElement(arr[:], 8)
-	InsertElement(arr[:], 9)
-
-	FindingMaxInArray(arr[:])
-	FindingMinInArray(arr[:])
-	// fmt.Println(arr)
+	takingInput()
 
 }
