cbee0b28360866fcfd87407c492d890b74627197
Sahibzada Hasanat Ahmad <sahibzadahasanatahmad@gmail.com>
2025-03-17 23:18:59 +0500

    updated

--- arraysAndSlices.go
+++ arraysAndSlices.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func Array() {
 	var marks [5]int = [5]int{1, 2, 4, 5}
+	// var marks = [5]int{1, 2, 4, 5}                   the 6th and 7th line are the same. 7th line is preferable
+	// marks := [5]int{1, 2, 4, 5} 					 the 6th and 8th line are the same. 8th line is preferable
 	fmt.Println(marks)
 	fmt.Println("Length of array is", len(marks))
 	fmt.Println("Capacity of array is", cap(marks))
